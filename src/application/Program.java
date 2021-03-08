b6e6feed06fb90cb658687255872b4aae6ff964c
Felipe Navas <felipefnavas@gmail.com>
2021-03-08 14:28:41 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Program {
+    public static void main(String[] Args){
+        sysout("oi");
+
+
+    }
+
+    public static void sysout(String s){
+        System.out.print(s);
+    }
+    public static void sysoutln(String s){
+        System.out.println(s);
+    }
+}
